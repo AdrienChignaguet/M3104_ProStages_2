c2ff41cecfe6da34d790cfc6849d6a7209d1e704
Adrien Chignaguet <achignaguet@iutbayonne.univ-pau.fr>
2021-12-16 11:43:22 +0100

    Mise en place des premieres routes

--- src/Controller/ProstagesController.php
+++ src/Controller/ProstagesController.php
@@ -17,4 +17,35 @@ class ProstagesController extends AbstractController
             'controller_name' => 'ProstagesController',
         ]);
     }
+
+    /**
+     * @Route("/entreprises", name="prostages_entreprises")
+     */
+    public function dirEntreprises(): Response
+    {
+        return $this->render('prostages/entreprises.html.twig', [
+            'controller_name' => 'ProstagesController',
+        ]);
+    }
+
+    /**
+     * @Route("/formation", name="prostages_formation")
+     */
+    public function dirFormation(): Response
+    {
+        return $this->render('prostages/formation.html.twig', [
+            'controller_name' => 'ProstagesController',
+        ]);
+    }
+
+    /**
+     * @Route("/stages/{id}", name="prostages_stages")
+     */
+    public function dirStages($id): Response
+    {
+        return $this->render('prostages/stages.html.twig', [
+            'controller_name' => 'ProstagesController',
+            'id' =>$id
+        ]);
+    }
 }
