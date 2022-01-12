5dd06930ed623f973784f30f2cc471140265be7d
Adrien Chignaguet <achignaguet@iutbayonne.univ-pau.fr>
2022-01-12 18:39:34 +0100

    Ajout de formations et d'une entreprise dans le jeu de test (faker)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -13,6 +13,9 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        //CREATION D'UN GENERATEUR DE DONNEES FAKER
+        $faker = \Faker\Factory::create('fr_FR');
+
         //ENTREPRISE
         $ent1 = new Entreprise();
         $ent1->setCode("ENT1");
@@ -23,6 +26,15 @@ class AppFixtures extends Fixture
 
         $manager->persist($ent1);
 
+        $ent2 = new Entreprise();
+        $ent2->setCode("ENT2");
+        $ent2->setActivite("Developpement Informatique");
+        $ent2->setAdresse($faker->regexify($faker->streetAddress.' '.$faker->postcode.' '.$faker->streetName));
+        $ent2->setNom($faker->companysuffix);
+        $ent2->setUrlSite("url.com");
+
+        $manager->persist($ent2);
+
         //FORMATION
         $for1 = new Formation();
         $for1->setCode("FOR1");
@@ -30,6 +42,22 @@ class AppFixtures extends Fixture
         $for1->setNomCourt("DUT Info");
 
         $manager->persist($for1);
+
+
+        $for2 = new Formation();
+        $for2->setCode("FOR2");
+        $for2->setNomLong("Licence Professionnelle Metiers du Numérique");
+        $for2->setNomCourt("LP NUM");
+
+        $manager->persist($for2);
+
+
+        $for3 = new Formation();
+        $for3->setCode("FOR3");
+        $for3->setNomLong("licence Professionnelle Programmation Avancee");
+        $for3->setNomCourt("LP PROG");
+
+        $manager->persist($for3);
 
         //STAGE
         $stageDevWeb = new Stage();
